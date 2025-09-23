b7f92f339085c10bf53d907878e83afa09128900
Serge Poliakov <poliakov.serega@gmail.com>
2025-09-23 22:24:17 +0300

    Tested id writing, date&time parsing

---
+++ CSVParser.Tests/OptionsTests/WrittingIdTests.cs
@@ -0,0 +1,98 @@
+﻿using CSVParser.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVParser.Tests.OptionsTests;
+
+public class WrittingIdTests
+{
+    private record TestIdEntInt
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    private record TestIdEntGuid
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    private record TestEntNoId
+    {
+        public string Name { get; set; }
+    }
+
+    [Theory]
+    [InlineData("Name", "James", "Abraham", "Sarah", "Olga")]
+    [InlineData("Id,Name", "5,James","6,Abraham","7,Sarah","8,Olga")]
+    public void WritingIntId(params string[] testStrings)
+    {
+        var parser = new CsvParser<TestIdEntInt>()
+            .WithHeaderParser(headerParser =>
+                headerParser.AddNamingPolicy(NameParsing.NameParsingStandarts.ExactMatch))
+            .WriteId()
+            .Build();
+
+        var stream = CSVTestUtils.StreamFromStrings(testStrings);
+        var result = parser.ParseStream(stream);
+
+        Assert.Equal(4, result.Count);
+        for (int i = 0; i < result.Count; i++)
+        {
+            Assert.Equal(i, result[i].Id);
+        }
+    }
+
+    [Theory]
+    [InlineData("Name", "James", "Abraham", "Sarah", "Olga")]
+    [InlineData("Id,Name", "5,James", "6,Abraham", "7,Sarah", "8,Olga")]
+    public void WritingGuidId(params string[] testStrings)
+    {
+        var parser = new CsvParser<TestIdEntGuid>()
+            .WithHeaderParser(headerParser =>
+                headerParser.AddNamingPolicy(NameParsing.NameParsingStandarts.ExactMatch))
+            .WriteId()
+            .Build();
+        var stream = CSVTestUtils.StreamFromStrings(testStrings);
+
+        var result = parser.ParseStream(stream);
+        Assert.Equal(4, result.Count);
+        for (int i = 0; i < result.Count; i++)
+        {
+            Assert.NotEqual(Guid.Empty, result[i].Id);
+        }
+    }
+
+    [Fact]
+    public void ThrowsOnNoIdFound()
+    {
+        Assert.Throws<CSVParserBuildingException>(() => new CsvParser<TestEntNoId>()
+            .WithHeaderParser(headerParser =>
+                headerParser.AddNamingPolicy(NameParsing.NameParsingStandarts.ExactMatch))
+            .WriteId()
+            .Build());
+    }
+
+    [Theory]
+    [InlineData("Id,Name", "5,James", "6,Abraham", "7,Sarah", "8,Olga")]
+    public void DoesNotOverrideIdIfPresent(params string[] testStrings)
+    {
+        var parser = new CsvParser<TestIdEntInt>()
+            .WithHeaderParser(headerParser =>
+                headerParser.AddNamingPolicy(NameParsing.NameParsingStandarts.ExactMatch))
+            .Build();
+        var stream = CSVTestUtils.StreamFromStrings(testStrings);
+
+        var result = parser.ParseStream(stream);
+
+        Assert.Equal(4, result.Count);
+        for (int i = 0; i < result.Count; i++)
+        {
+            Assert.Equal(i + 5, result[i].Id);
+        }
+    }
+}
---
+++ CSVParser.Tests/SpecifiqueTypesTests/DateAndTimeTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVParser.Tests.SpecifiqueTypesTests;
+
+public class DateAndTimeTests
+{
+    public string[] testString =
+        ["DateOnly,TimeSpan,DateTime",
+        "2023-12-31,15:30:45,2023-12-31 23:59:59"];
+    public DateOnly expectedDateOnly = new DateOnly(2023, 12, 31);
+    public TimeSpan expectedTimeSpan = new TimeSpan(15, 30, 45);
+    public DateTime expectedDateTime = new DateTime(2023, 12, 31, 23, 59, 59);
+
+    private record TestDateAndTimeEntity
+    {
+        public DateOnly DateOnly { get; set; }
+        public TimeSpan TimeSpan { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+
+    [Fact]
+    public void DateAndTimeParsingWithFormatSpecified()
+    {
+        var parser = new CSVParser.CsvParser<TestDateAndTimeEntity>()
+            .WithFormat(typeof(DateOnly), "yyyy-MM-dd")
+            .WithFormat(typeof(TimeSpan), "hh\\:mm\\:ss")
+            .WithFormat(typeof(DateTime), "yyyy-MM-dd HH:mm:ss")
+            .WithHeaderParser(headerParser =>
+                headerParser.AddNamingPolicy(CSVParser.NameParsing.NameParsingStandarts.ExactMatch))
+            .Build();
+
+        var stream = CSVTestUtils.StreamFromStrings(testString);
+
+        var result = parser.ParseStream(stream);
+        
+        Assert.Single(result);
+        var entity = result[0];
+        Assert.Equal(expectedDateOnly, entity.DateOnly);
+        Assert.Equal(expectedTimeSpan, entity.TimeSpan);
+        Assert.Equal(expectedDateTime, entity.DateTime);
+    }
+}
